9e7c66924894b4035caf13284be32838e79a2f56
Pluggito <pluggitoshfl5050@gmail.com>
2025-10-18 11:49:49 +0100

    updated the servers

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  // console.log(`Server is running on port ${port}`);
 });
